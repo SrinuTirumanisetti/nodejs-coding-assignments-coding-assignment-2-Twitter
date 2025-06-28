89fc1b53a28f417eeab72bea80dc5fbb180aba19
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-06-28 13:02:55 +0000

    User Registration completed

--- app.js
+++ app.js
@@ -27,3 +27,29 @@ const InitializeDBAndServer = async () => {
 };
 
 InitializeDBAndServer();
+
+app.post("/register", async (request, response) => {
+  const { username, password, name, gender } = request.body;
+
+  const selectUserQuery = `SELECT * FROM user WHERE username = ?;`;
+  const dbUser = await db.get(selectUserQuery, [username]);
+
+  if (dbUser !== undefined) {
+    response.status(400);
+    response.send("User already exists");
+  } else {
+    if (password.length < 6) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const addUserQuery = `
+        INSERT INTO user (username, password, name, gender)
+        VALUES (?, ?, ?, ?);`;
+      await db.run(addUserQuery, [username, hashedPassword, name, gender]);
+
+      response.status(200);
+      response.send("User created successfully");
+    }
+  }
+});
